1cd0c5305bfc8663a58cb0c9c75b2c504a04b557
Jared Cuffe <jcuffe@gmail.com>
2018-05-16 12:05:50 -0700

    Implemented login route using passport as middleware

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const passport = require('passport')
 const User = require('../users/User');
 
 router.post('/register', function(req, res) {
@@ -10,5 +10,10 @@ router.post('/register', function(req, res) {
     })
     .catch(err => res.status(500).json(err));
 });
+
+router.post('/login', passport.authenticate('local', {session: false}), (req, res) => {
+  console.log('params', req.body)
+  res.send({user: req.user})
+})
 
 module.exports = router;
