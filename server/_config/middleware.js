17988a67903b12b2c6087215a9dee5ae5ca21476
Jared Cuffe <jcuffe@gmail.com>
2018-05-16 12:05:29 -0700

    Implemented authentication strategy

--- server/_config/middleware.js
+++ server/_config/middleware.js
@@ -2,10 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../users/User')
+
+passport.use(new LocalStrategy((username, password, done) => {
+  User.findOne({ username })
+    .then(user => user.validatePassword(password)
+      .then(validated => validated
+        ? done(null, user)
+        : done(null, 'error')))
+}))
 
 module.exports = function(server) {
   server.use(helmet());
   server.use(morgan('dev'));
   server.use(express.json());
   server.use(cors());
+  server.use(passport.initialize())
 };
